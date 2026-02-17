d9b9734ce4c4d634e4c394b617d9060c221047d7
Jorge Ceballos Gomez <ceballosjorge84@gmail.com>
2026-02-17 08:50:20 -0500

    Contains Duplicate II & Valid Anagram Solutions

---
+++ arrays/validAnagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagramn(String s, String t){
+        //checkign to se iof both strigns re same length becuase if not, return false since they cant be anagaram
+        if (s.length() != t.length()) return false;
+
+        //create an array to count character frequencies, 26 letters for alphabet
+        int[] charCounts = new int[26];
+
+        //increment count for each character in s and decrement for each in t
+        for (int i= o; i < s.length(); i++){
+            charCounts[s.charAt(i) - 'a']++;
+            charCounts[t.charAt(i) - 'a']--;
+        }
+
+        //now check if all counts are zero. if count is not 0 then should return fals since it is not anagram
+        for( int count : charCounts ){
+            if (count != 0) return false;
+        }
+         return true;
+    }
+}
